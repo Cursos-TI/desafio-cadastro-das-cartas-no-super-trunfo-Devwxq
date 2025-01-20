b3fce407607af6b58ecf7cedb5f9f973e9ce32fa
Thales Santos <thalesantos79@gmail.com>
2025-01-20 10:55:42 +0000

    criei o primeiro printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,15 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char codigocity;
+    char nomecity;
+    float popucao;
+    float area;
+    float pib;
+    int pontostour;
+
+    printf("## Cadastro de cartas SuperTrunfo ##\n");
+    
 
     return 0;
 }
