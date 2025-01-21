315a21d9c393bb2a509ab15128fdfcf7e757dc4e
Thales Santos <thalesantos79@gmail.com>
2025-01-21 13:27:34 +0000

    Att 1.0.3

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,53 +18,60 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
     char letra = 'A';
-    int numero = '1';
-    char nome [30] = 'Brasil';
-    char cidade [30] = 'Rio de Janeiro';
-    float populacao = '12345678';
-    double area = '85547893.85';
-    double pib = '12345678.09';
-    int pontos = '5';
+    int numero = 1;
+    char nome[20] = "Brasil";
+    char cidade[20] = "Rio de Janeiro";
+    int populacao = 200000000;
+    double area = 875.63;
+    double pib = 53078.23;
+    int pontos = 2;
 
-    //Título
     printf("## Cadastro de cartas SuperTrunfo ##\n");
-        
+    
+    //Coleta de dados para cadastro de uma nova carta.
     printf("Insira uma letra para o código da nova carta\n");
     scanf("%c", &letra);
 
-    printf("Insira um número para o código nova carta\n");
+    printf("Insira um número para o código da nova carta\n");
     scanf(" %d", &numero);
-    printf("Codigo da carta: %c%d\n", letra, numero);
+    printf("Código cadastrado com sucesso!\n");
     
     printf("Insira o nome do país\n");
-    scanf(" %s", &nome);
-    printf("País cadastrado com sucesso!\n");   
+    scanf(" %s", nome);
+    printf("País cadastrado com sucesso!\n"); 
 
-    printf("Insira o nome da cidade\n");
-    scanf(" %s", &cidade);
-    printf("Cidade cadastrada com sucesso!\n");    
+    printf("Insira o nome da Cidade\n");
+    scanf(" %s", cidade);
+    printf("Cidade cadastrada com sucesso!\n");       
 
     printf("Insira o número de população desta cidade\n");
     scanf("%d", &populacao);
     printf("População cadastrada com sucesso!\n");
 
-    printf("Insira a extenção territorial deste país (KM²)\n");
+    printf("Insira a extensão territorial deste país\n");
     scanf("%lf", &area);
+    printf("Extensão territorial cadastrada com sucesso!\n");
 
     printf("Insira o PIB da cidade\n");
     scanf("%lf", &pib);
-    printf("PIB cadastrado!\n");
+    printf("PIB cadastrado com sucesso!\n");
 
     printf("Insira a quantidade de pontos turísticos desta cidade\n");
     scanf("%d", &pontos);
+    printf("Todos os seus dados foram cadastrados\n");
+    
+    printf("#########################################\n");
+    printf("#########################################\n");
 
+    //Apresentação dos dados da nova carta cadastrada.
+    printf("\n## Nova carta cadastrada! ##\n");
     printf("Código da carta: %c%d\n", letra, numero);
     printf("País: %s\n", nome);
     printf("Cidade: %s\n", cidade);
-    printf("População: %f\n", populacao);
-    printf("Extenção Territorial: %.2f KM²\n", area);
-    printf("PIB R$: %e\n", pib);
-    printf("Qnt. Pontos Turísticos: %d\n", pontos);
+    printf("População: %d\n", populacao);
+    printf("Extensão territorial: %.2lf km²\n", area);
+    printf("PIB: R$%.2lf\n", pib);
+    printf("Pontos turísticos: %d\n", pontos);
 
     return 0;
 }
